b55f3f44dbc148db9c527ad09aee3fa3e7e5165e
Dinesh <skillupwithdinesh@gmail.com>
2026-04-18 07:37:14 +0530

    Added simple java workflow

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -6,6 +6,6 @@ public class App {
     }
 
     public static int add(int a, int b) {
-        return a - b;
+        return a + b;
     }
 }
--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -9,4 +9,14 @@ public class AppTest {
     void testAdd() {
         assertEquals(5, App.add(2, 3));
     }
+
+    @Test
+    void testAdd2() {
+        assertEquals(50, App.add(20, 30));
+    }
+
+    @Test
+    void testAdd3() {
+        assertEquals(15, App.add(20, -5));
+    }
 }
